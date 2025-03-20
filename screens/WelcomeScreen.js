68d3ac5f53ab7c2dbb0e0f053079477835ba7b0b
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-03-20 10:25:17 +0100

    Get protected data

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+	const [fetchedMessages, setFetchedMessages] = useState("");
+	const authCtx = useContext(AuthContext);
+	const token = authCtx.token;
+
+	useEffect(() => {
+		axios
+			.get(
+				`https://expense-tracker-a9ce8-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${token}`,
+			)
+			.then((response) => {
+				console.log(response);
+				setFetchedMessages(response.data);
+			});
+	}, [token]);
+
 	return (
 		<View style={styles.rootContainer}>
 			<Text style={styles.title}>Welcome!</Text>
 			<Text>You authenticated successfully!</Text>
+			<Text>{fetchedMessages}</Text>
 		</View>
 	);
 }
